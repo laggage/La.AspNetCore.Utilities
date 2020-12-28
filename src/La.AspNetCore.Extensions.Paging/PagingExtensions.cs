a37b443c74e1d881355c1bb5138db2d1c99cf715
laggage <laggage-la@outlook.com>
2020-12-28 14:22:40 +0800

    Remove PageAsync in La.AspNetCore.Extensions.Paging

--- src/La.AspNetCore.Extensions.Paging/PagingExtensions.cs
+++ src/La.AspNetCore.Extensions.Paging/PagingExtensions.cs
@@ -1,6 +1,7 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
-using System.Threading.Tasks;
 
 namespace La.AspNetCore.Extensions.Paging
 {
@@ -8,68 +9,44 @@ namespace La.AspNetCore.Extensions.Paging
     {
         /// <summary>
         /// 使用 <see cref="PageModel.DefaultPageSize"/> 定义的默认页尺寸对数据源进行分页
-        /// 如果数据源是延迟加载的集合类型, 那么请考虑使用 <see cref="PageAsync{T}(IEnumerable{T}, int)"/>
         /// </summary>
         /// <param name="source">数据源</param>
         /// <param name="pageIndex">页码</param>
-        /// <returns> <see cref="PageModel{T}"/> </returns>
-        public static PageModel<T> Page<T>(
-            this IEnumerable<T> source,
-            int pageIndex)
-            where T : class
-        {
-            return source.Page(pageIndex, PageModel.DefaultPageSize);
-        }
-
-        /// <summary>
-        /// 对数据源进行分页, 如果数据源是延迟加载的集合类型, 那么请考虑使用 <see cref="PageAsync{T}(IEnumerable{T}, int, int)"/>
-        /// </summary>
-        /// <param name="source">数据源</param>
-        /// <param name="pageIndex">页码</param>
-        /// <param name="pageSize">页尺寸</param>
+        /// <param name="totalItemsCount">default to null, set to null will use source.Count()</param>
         /// <returns> <see cref="PageModel{T}"/> </returns>
         public static PageModel<T> Page<T>(
             this IEnumerable<T> source,
             int pageIndex,
-            int pageSize)
+            int? totalItemsCount = null)
             where T : class
         {
-            int totalItemsCount = source.Count();
+            return source.Page(pageIndex, PageModel.DefaultPageSize, totalItemsCount);
+        }
+
+        /// <summary>
+        /// 对数据源进行分页
+        /// </summary>
+        /// <param name="source">数据源</param>
+        /// <param name="pageIndex">页码</param>
+        /// <param name="pageSize">页尺寸</param>
+        /// <param name="totalItemsCount">default to null, set to null will use source.Count()</param>
+        /// <returns> <see cref="PageModel{T}"/> </returns>
+        [SuppressMessage("ReSharper", "PossibleMultipleEnumeration")]
+        [SuppressMessage("ReSharper", "MemberCanBePrivate.Global")]
+        public static PageModel<T> Page<T>(
+            this IEnumerable<T> source,
+            int pageIndex,
+            int pageSize,
+            int? totalItemsCount = null)
+            where T : class
+        {
+            source = source ?? throw new ArgumentNullException(nameof(source));
+            totalItemsCount = totalItemsCount ?? source.Count();
             var data = source.Skip((pageIndex - 1) * pageSize)
                 .Take(pageSize)
                 .ToList();
             return new PageModel<T>(
-                pageIndex, pageSize, totalItemsCount, data);
-        }
-
-        /// <summary>
-        /// 对数据源进行分页, 如果数据源不是延迟的加载的集合, 请勿使用此异步方法
-        /// </summary>
-        /// <param name="source">数据源</param>
-        /// <param name="pageIndex">页码</param>
-        /// <param name="pageSize">页尺寸</param>
-        /// <returns> <see cref="PageModel{T}"/> </returns>
-        public static Task<PageModel<T>> PageAsync<T>(
-            this IEnumerable<T> source,
-            int pageIndex,
-            int pageSize)
-            where T : class
-        {
-            return Task.Run(() => source.Page(pageIndex, pageSize));
-        }
-
-        /// <summary>
-        /// 使用 <see cref="PageModel.DefaultPageSize"/> 定义的默认页尺寸对数据源进行分页, 如果数据源不是延迟的加载的集合, 请勿使用此异步方法
-        /// </summary>
-        /// <param name="source">数据源</param>
-        /// <param name="pageIndex">页码</param>
-        /// <returns> <see cref="PageModel{T}"/> </returns>
-        public static Task<PageModel<T>> PageAsync<T>(
-            this IEnumerable<T> source,
-            int pageIndex)
-            where T : class
-        {
-            return Task.Run(() => source.Page(pageIndex));
+                pageIndex, pageSize, totalItemsCount.Value, data);
         }
     }
 }
--- src/tests/La.AspNetCore.Extensions.Test/PagingTests/PageExtensionsTest.cs
+++ src/tests/La.AspNetCore.Extensions.Test/PagingTests/PageExtensionsTest.cs
@@ -26,32 +26,5 @@ namespace La.AspNetCore.Extensions.Test.PagingTests
                 (TestPageData.Items.Length / PageModel.DefaultPageSize) + (TestPageData.Items.Length % PageModel.DefaultPageSize > 0 ? 1 : 0),
                 pagedData.PageCount);
         }
-
-        [Fact]
-        public async Task PageAsyncTest()
-        {
-            // Arrange
-            string[] data = TestPageData.Items;
-            int pageSize = 5;
-
-            // Act
-            PageModel<string> pagedData = await data.PageAsync(1);
-            PageModel<string> pagedData2 = await data.PageAsync(2, pageSize);
-
-            // Assert
-            Assert.NotEmpty(pagedData.Items);
-            Assert.Equal(PageModel.DefaultPageSize, pagedData.ItemsCount);
-            Assert.Equal(PageModel.DefaultPageSize, pagedData.PageSize);
-            Assert.Equal(TestPageData.Items.Length, pagedData.TotalItemsCount);
-            Assert.False(pagedData.HasPrevious);
-            Assert.True(pagedData.HasNext);
-
-            Assert.NotEmpty(pagedData2.Items);
-            Assert.Equal(pageSize, pagedData2.ItemsCount);
-            Assert.Equal(pageSize, pagedData2.PageSize);
-            Assert.Equal(TestPageData.Items.Length, pagedData2.TotalItemsCount);
-            Assert.True(pagedData2.HasPrevious);
-            Assert.True(pagedData2.HasNext);
-        }
     }
 }
